fffcdf7dbbc4d911b2e99a8a74597c47ae5b3aed
Pierre-Yves Ricau <py@squareup.com>
2023-11-01 13:46:24 -0700

    Initial webview loading impl

--- app/src/main/java/com/example/chromiumwebviewleak/MainActivity.kt
+++ app/src/main/java/com/example/chromiumwebviewleak/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.chromiumwebviewleak
 
+import android.R.style
+import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +32,43 @@ class MainActivity : ComponentActivity() {
         }
       }
     }
+
+    // TODO Try with alert dialog.
+    val dialog = Dialog(this, style.Theme_Black_NoTitleBar)
+    val contentView = View.inflate(this, R.layout.browser, null)
+
+
+    val webView = contentView.findViewById<WebView>(R.id.webview)
+    val progressBar = contentView.findViewById<ProgressBar>(R.id.web_progress_bar)
+
+    webView.webViewClient = SlowLoadingWebviewClient()
+    webView.webChromeClient = object : WebChromeClient() {
+      override fun onProgressChanged(
+        view: WebView,
+        newProgress: Int
+      ) {
+        progressBar.progress = newProgress
+      }
+    }
+    webView.settings.javaScriptEnabled = true
+    webView.settings.domStorageEnabled = true
+    webView.isVerticalScrollBarEnabled = false
+    webView.loadUrl("https://squareup.com/us/en/legal/general/ua")
+    dialog.setContentView(contentView)
+    dialog.setCanceledOnTouchOutside(true)
+    dialog.show()
+  }
+}
+
+class SlowLoadingWebviewClient : WebViewClient() {
+  override fun shouldInterceptRequest(
+    view: WebView,
+    request: WebResourceRequest
+  ): WebResourceResponse? {
+    if (request.url.toString().endsWith(".js")) {
+      Thread.sleep(10_000)
+    }
+    return null
   }
 }
 
